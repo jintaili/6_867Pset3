2f62ea47f89f9fb6188436cb5b6e593847b3d7a3
jintaili <jintaili@mit.edu>
2017-11-12 22:34:05 -0500

    Modified Feedforward NN with 1 hidden layer of customizable number of nodes

--- pset3_1_b.py
+++ pset3_1_b.py
@@ -48,7 +48,7 @@ def calculate_error(model, X, y):
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
     # Forward propagation to calculate our predictions
     z1 = X.dot(W1) + b1
-    a1 = tanh_activation(z1)
+    a1 = relu_activation(z1)
     z2 = a1.dot(W2) + b2
     exp_scores = np.exp(z2)
     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
@@ -61,7 +61,7 @@ def calculate_loss_batch(model, X, y):
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
     # Forward propagation to calculate our predictions
     z1 = X.dot(W1) + b1
-    a1 = tanh_activation(z1)
+    a1 = relu_activation(z1)
     z2 = a1.dot(W2) + b2
     exp_scores = np.exp(z2)
     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
@@ -76,7 +76,7 @@ def calculate_loss_stoch(model, x_pt, y_pt):
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
     # Forward propagation to calculate our predictions
     z1 = x_pt.dot(W1) + b1
-    a1 = tanh_activation(z1)
+    a1 = relu_activation(z1)
     z2 = a1.dot(W2) + b2
     exp_scores = np.exp(z2)
     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
@@ -108,7 +108,7 @@ def build_model(nn_hdim, X, y, num_passes=20000, print_loss=False):
 
         # Forward propagation
         z1 = X.dot(W1) + np.repeat(b1, num_examples, 0)
-        a1 = tanh_activation(z1)
+        a1 = relu_activation(z1)
         z2 = a1.dot(W2) + np.repeat(b2, num_examples, 0)
         exp_scores = np.exp(z2)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
@@ -118,7 +118,7 @@ def build_model(nn_hdim, X, y, num_passes=20000, print_loss=False):
         delta3[range(num_examples), y] -= 1
         dW2 = (a1.T).dot(delta3)
         db2 = np.sum(delta3, axis=0, keepdims=True)
-        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        delta2 = delta3.dot(W2.T).dot(np.diag(np.reshape((z1>0)*1, -1)))
         dW1 = np.dot(X.T, delta2)
         db1 = np.sum(delta2, axis=0)
 
@@ -145,7 +145,7 @@ def build_model(nn_hdim, X, y, num_passes=20000, print_loss=False):
 
     return model
 
-def build_model_SGD(nn_hdim, X, y, num_passes=500, print_loss=False):
+def build_model_SGD_tanh(nn_hdim, X, y, num_passes=500, print_loss=False):
     # Initialize the parameters to random values. We need to learn these.
     num_examples = np.size(X, 0)
     loss_T = []
@@ -212,6 +212,74 @@ def build_model_SGD(nn_hdim, X, y, num_passes=500, print_loss=False):
 
     return (model,loss_T)
 
-rt = build_model_SGD(3, X, y, print_loss=True)
+def build_model_SGD(nn_hdim, X, y, num_passes=500, print_loss=False):
+    # Initialize the parameters to random values. We need to learn these.
+    num_examples = np.size(X, 0)
+    loss_T = []
+    #np.random.seed(0)
+    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_output_dim))
+
+    # This is what we return at the end
+    model = {}
+
+    # Gradient descent. For each batch...
+    for i in range(0, num_passes):
+
+        indices_examples = range(num_examples)
+
+        for j in range(num_examples):
+
+            pt = np.random.choice(indices_examples,replace=False)
+            x_pt = np.reshape(X[pt,:],(1,-1))
+            y_pt = y[pt]
+
+            # Forward propagation
+            z1 = x_pt.dot(W1) + b1
+            a1 = relu_activation(z1)
+            z2 = a1.dot(W2) + b2
+            exp_scores = np.exp(z2)
+            probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+            # Backpropagation
+            delta_L = np.copy(probs)
+            delta_L[0,y_pt] -= 1
+            dW2 = (a1.T).dot(delta_L)
+            db2 = np.sum(delta_L, axis=0, keepdims=True)
+            delta2 = delta_L.dot(W2.T).dot(np.diag(np.reshape((z1>0)*1,-1)))
+            dW1 = np.dot(x_pt.T, delta2)
+            db1 = np.sum(delta2, axis=0)
+
+            # Add regularization terms (b1 and b2 don't have regularization terms)
+            dW2 += reg_lambda * W2
+            dW1 += reg_lambda * W1
+
+            # Gradient descent parameter update
+            epsilon = learning_rate_SGD((i*num_examples + j+1)/2000, tau0 = 0.3, kap = 0.55)
+            epsilon = 0.01
+            W1 += -epsilon * dW1
+            b1 += -epsilon * db1
+            W2 += -epsilon * dW2
+            b2 += -epsilon * db2
+
+            # Assign new parameters to the model
+            model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+        # Optionally print the loss.
+        # This is expensive because it uses the whole dataset, so we don't want to do it too often.
+        if print_loss and i % 10 == 0:
+            loss_T.append(calculate_loss_batch(model, X, y))
+            print(
+                "Loss after epoch %i: %f" % (i, loss_T[-1])
+            )
+            print(
+                "Error after epoch %i: %f" % (i, calculate_error(model, X, y))
+            )
+
+    return (model,loss_T)
+
+rt = build_model_SGD(100, X, y, print_loss=True)
 model = rt[0]
 plt.plot(rt[1])
